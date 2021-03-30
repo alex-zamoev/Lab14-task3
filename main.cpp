98219146af594bbfa899330626e23c796bdb1b98
alex-zamoev <alex.zamoev@yandex.ru>
2021-03-30 17:35:02 +0800

    release2

--- main.cpp
+++ main.cpp
@@ -19,62 +19,54 @@ bool check_for_symbols(char sym){
     return temp;
 }
 
-bool startTest(string email){
-    bool test = true;
 
 
-    bool findAtSign = false;
-    int lengthFirstPart = 0, lengthSecondPart = 0;
+int positionAtSign(string addr){
+    int pos;
     int countAtSign = 0;
-    bool checkForSymbol = false;
-    bool checkForSpecSymbol = false;
-
-    checkForSymbol = check_for_symbols(email[0]);
-    checkForSpecSymbol = (checkForSymbol) ? true : check_for_spec_symbols(email[0]);
-
-    if (email[0] == '@' || email[0] == '.' || email[email.length() - 1] == '.' || ((!checkForSymbol) && (!checkForSpecSymbol))) test = false;
-    else{
-        for(int i = 1; i < email.length() - 1; i++){
-            checkForSymbol = check_for_symbols(email[i]);
-            checkForSpecSymbol = (checkForSymbol) ? true : check_for_spec_symbols(email[i]);
-
-            if(email[i] == '@') {
-                countAtSign++;
-                findAtSign = true;
-                if(countAtSign > 1) {
-                    test = false;
-                    break;
-                }
-                lengthFirstPart = i;
-                lengthSecondPart = email.length() - lengthFirstPart - 1;
-            }
-
-            if(!findAtSign){
-                if(!checkForSymbol && !checkForSpecSymbol){
-                    test = false;
-                    break;
-                }
-            }else{
-                if(!checkForSymbol && checkForSpecSymbol){
-                    test = false;
-                    break;
-                }
-            }
-            if(email[i] == '.' && email[i-1] == '.') {
-                test = false;
-                break;
-            }
+    for(int i = 0; i < addr.length(); i++){
+        if(addr[i] == '@'){
+            pos = i;
+            countAtSign++;
         }
     }
-    return test;
+    if(countAtSign > 1) pos = 0;
+    return pos;
 }
 
 int main(){
     string email;
+    int posAtSign = 0;
+    bool mainFlag = true;
     cout << "Enter email address: " << endl;
     cin >> email;
 
-    (startTest(email)) ? cout << "Yes\n" : cout << "No\n";
+    posAtSign = positionAtSign(email);
+
+    if (email[0] == '.' || email[email.length() - 1] == '.') mainFlag = false;
+    else{
+        if(posAtSign == 0) mainFlag = false;
+        else{
+            for(int i = 0; i < posAtSign; i++){
+                mainFlag = check_for_symbols(email[i]) || check_for_spec_symbols(email[i]);
+                if(!mainFlag) break;
+                if(email[i] == '.' && email[i+1] == '.'){
+                    mainFlag = false;
+                    break;
+                }
+            }
+            for(int i = posAtSign + 1; i < email.length(); i++){
+                mainFlag = check_for_symbols(email[i]);
+                if(!mainFlag) break;
+                if(email[i] == '.' && email[i-1] == '.'){
+                    mainFlag = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    (mainFlag) ? cout << "Yes\n" : cout << "No\n";
 
     return 0;
 
