b4aaf8a79bd7b3573d103bb74cb88a8f23770382
alex-zamoev <alex.zamoev@yandex.ru>
2021-04-08 12:19:34 +0800

    fix-08.04.2021

--- main.cpp
+++ main.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
-
 using namespace std;
 
 bool check_for_spec_symbols(char sym){
-    bool temp = false;
     string origin = "!#$%&'*+-/=?^_`{|}~";
     for(int j = 0; j < origin.length(); j++){
-        if(sym == origin[j]) temp = true;
-        break;
+        if(sym == origin[j]) return true;
     }
-    return temp;
+    return false;
 }
 
 bool check_for_symbols(char sym){
-    bool temp = true;
-    if((sym >= 'A' && sym <= 'Z') || (sym >= 'a' && sym <= 'z') || (sym >= '0' && sym <= '9') || sym == '-' || sym == '.') temp = true;
-    else temp = false;
-    return temp;
+    if((sym >= 'A' && sym <= 'Z') || (sym >= 'a' && sym <= 'z') ||
+       (sym >= '0' && sym <= '9') || sym == '-' || sym == '.') return true;
+    else return false;
 }
-
-
 
 int positionAtSign(string addr){
     int pos;
@@ -38,7 +32,8 @@ int main(){
     string email;
     int posAtSign = 0;
     int leftLength, rightLength;
-    bool mainFlag = true;
+    bool leftSideFlag = true;
+    bool rightSideFlag = true;
     cout << "Enter email address: " << endl;
     cin >> email;
 
@@ -46,33 +41,32 @@ int main(){
     leftLength = posAtSign;
     rightLength = email.length() - posAtSign;
 
-    if (email[0] == '.' || email[email.length() - 1] == '.' || leftLength > 64 || rightLength > 63) mainFlag = false;
+    if (email[0] == '.' || email[email.length() - 1] == '.' || leftLength > 64 || rightLength > 63) leftSideFlag = false;
     else{
-        if(posAtSign == 0) mainFlag = false;
+        if(posAtSign == 0) leftSideFlag = false;
         else{
             for(int i = 0; i < posAtSign; i++){
-                mainFlag = check_for_symbols(email[i]) || check_for_spec_symbols(email[i]);
-                if(!mainFlag) break;
+                leftSideFlag = check_for_symbols(email[i]) || check_for_spec_symbols(email[i]);
+                if(!leftSideFlag) break;
                 if(email[i] == '.' && email[i+1] == '.'){
-                    mainFlag = false;
+                    leftSideFlag = false;
                     break;
                 }
             }
             for(int i = posAtSign + 1; i < email.length(); i++){
-                mainFlag = check_for_symbols(email[i]);
-                if(!mainFlag) break;
+                rightSideFlag = check_for_symbols(email[i]);
+                if(rightSideFlag) break;
                 if(email[i] == '.' && email[i-1] == '.'){
-                    mainFlag = false;
+                    rightSideFlag = false;
                     break;
                 }
             }
         }
     }
 
-    (mainFlag) ? cout << "Yes\n" : cout << "No\n";
+    (leftSideFlag && rightSideFlag) ? cout << "Yes\n" : cout << "No\n";
 
     return 0;
-
 }
 
 
